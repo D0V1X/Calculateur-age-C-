5b9675951c3f1f215a0712e0227c9416c89261eb
Dorian Gozel <Dorian.gozel@gmail.com>
2017-12-14 14:13:44 +0100

    Switch fini, ajout de deux fonctionnalités (date/heure)

--- Calculateur_age/Calculateur_age/Program.cs
+++ Calculateur_age/Calculateur_age/Program.cs
@@ -28,22 +28,92 @@ namespace Calculateur_age
                 Console.WriteLine("************************************************************");
                 Console.WriteLine("Pour calculer votre âge en jours, tapez [1] !");
                 Console.WriteLine("Pour savoir l'heure, tapez [2] !");
-                Console.WriteLine("Pour quitter le programme, tapez [3] !");
+                Console.WriteLine("Pour savoir la date actuelle, tapez [3] !");
+                Console.WriteLine("Pour quitter le programme, tapez [4] !");
                 Console.WriteLine("************************************************************");
                 cSaisie = Console.ReadKey().KeyChar;
 
                 switch (cSaisie)
                 {
                     case '1':
+                        Console.Clear();
+                        do
+                        {
+                            Console.WriteLine("*********************************************************************");
+                            Console.WriteLine("Quel est votre jour de naissance ? ");
+                            string strJour = Console.ReadLine();
+                            Console.WriteLine("*********************************************************************");
+                            iday = Convert.ToInt32(strJour);
+                        }
+                        while (iday < 1 || iday > 31);
+
+                        do
+                        {
+                            Console.WriteLine("***********************************");
+                            Console.WriteLine("Quel est votre mois de naissance ? ", strNom);
+                            string strMois = Console.ReadLine();
+                            Console.WriteLine("***********************************");
+                            imonth = Convert.ToInt32(strMois);
+                        }
+                        while (imonth < 1 || imonth > 12);
+
+                        do
+                        {
+                            Console.WriteLine("************************************");
+                            Console.WriteLine("Quel est votre année de naissance ? ", strNom);
+                            string strAnnee = Console.ReadLine();
+                            Console.WriteLine("************************************");
+                            iyear = Convert.ToInt32(strAnnee);
+                        }
+                        while (imonth < 1);
+
+                        DateTime myBirthday = new DateTime(iyear, imonth, iday);
+                        TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+
+                        Console.Clear();
+                        Console.WriteLine("**************************************************************************************************************");
+                        Console.WriteLine("Vous êtes âgé de {0} jours ! Vous pouvez désormais fêter votre journiversaire avec vos amis et votre famille !", Math.Floor(myAge.TotalDays));
+                        Console.WriteLine("**************************************************************************************************************");
+                        Console.WriteLine("Pressez une touche pour continuer ...");
+                        Console.ReadKey();
                         break;
 
                     case '2':
+                        Console.Clear();
+                        DateTime TimeSet = new DateTime();
+                        TimeSpan Time = DateTime.Now.Subtract(TimeSet);
+
+                        Console.WriteLine("**************************************************************************************************************");
+                        Console.WriteLine("Il est {0}:{1}:{2}", (Time.Hours), (Time.Minutes), (Time.Seconds));
+                        Console.WriteLine("**************************************************************************************************************");
+                        Console.WriteLine("Pressez une touche pour continuer ...");
+                        Console.ReadKey();
                         break;
 
                     case '3':
+                        Console.Clear();
+                        Console.WriteLine("**************************************************************************************************************");
+                        Console.WriteLine("Nous sommes le {0}.{1}.{2}", DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
+                        Console.WriteLine("**************************************************************************************************************");
+                        Console.WriteLine("Pressez une touche pour continuer ...");
+                        Console.ReadKey();
                         break;
 
                     case '4':
+                        Console.Clear();
+                        Console.WriteLine("*****************************************");
+                        Console.WriteLine("Voulez-vous vraiment quitter ??? [o/n]");
+                        cSaisie = Console.ReadKey().KeyChar;
+
+                        if (cSaisie == 'o')
+                        {
+                            Console.Clear();
+                            Console.WriteLine("*****************************************");
+                            Console.WriteLine("***************Aurevoir...***************");
+                            Console.WriteLine("*****************************************");
+                            System.Threading.Thread.Sleep(1000);
+                            Environment.Exit(0);
+                        }
                         break;
                 }
             }
